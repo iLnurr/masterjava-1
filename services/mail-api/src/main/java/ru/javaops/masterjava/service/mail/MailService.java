a98f3f7dfcb64b37d914a031b3d639b8d44fafa3
Grigory Kislin <javawebinar@yandex.ru>
2016-11-28 22:27:59 +0300

    7_Customize_WSDL

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailService.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailService.java
@@ -9,7 +9,11 @@ import java.util.List;
  * gkislin
  * 15.11.2016
  */
-@WebService
+@WebService(targetNamespace = "http://mail.javaops.ru/")
+//@SOAPBinding(
+//        style = SOAPBinding.Style.DOCUMENT,
+//        use= SOAPBinding.Use.LITERAL,
+//        parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public interface MailService {
 
     @WebMethod
--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
@@ -7,7 +7,9 @@ import java.util.List;
  * gkislin
  * 15.11.2016
  */
-@WebService(endpointInterface = "ru.javaops.masterjava.service.mail.MailService")
+@WebService(endpointInterface = "ru.javaops.masterjava.service.mail.MailService", targetNamespace = "http://mail.javaops.ru/"
+//          , wsdlLocation = "WEB-INF/wsdl/mailService.wsdl"
+)
 public class MailServiceImpl implements MailService {
     public void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
         MailSender.sendMail(to, cc, subject, body);
