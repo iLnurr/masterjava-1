d047fa3c288758e16135a92efe0aaa97e82c26a5
Grigory Kislin <javawebinar@yandex.ru>
2016-12-19 23:44:57 +0300

    Add WebService Exception

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -1,11 +1,13 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableSet;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * gkislin
  * 28.11.2016
  */
+@Slf4j
 public class MailWSClientMain {
     public static void main(String[] args) {
         ImmutableSet<Addressee> addressees = ImmutableSet.of(
@@ -13,6 +15,11 @@ public class MailWSClientMain {
                 new Addressee("Мастер Java <masterjava@javaops.ru>"),
                 new Addressee("Bad Email <bad_email.ru>"));
 
-        MailWSClient.sendIndividualMails(addressees, "Subject", "Body");
+        try {
+            String state = MailWSClient.sendBulkMail(addressees, ImmutableSet.of(), "Subject", "Body");
+            System.out.println(state);
+        } catch (Throwable e) {
+            log.error(e.toString(), e);
+        }
     }
 }
